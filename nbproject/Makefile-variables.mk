fc0ab9c980bd92974732b91dd14b799481f5d971
Katsumi <kmorimatsu@sourceforge.jp>
2019-07-15 11:57:57 -0700

    Add Makefile

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=kmz80midi.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/kmz80midi.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=kmz80midi.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/kmz80midi.x.tar
